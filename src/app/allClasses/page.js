bcf0adf41efe93b742e4f38b2f23d380a18b4c44
joy-mahmud <joymahmudiucse@gmail.com>
2024-02-21 16:01:05 +0600

    all classes route created and demo card shown for all the classes

--- src/app/allClasses/page.js
+++ src/app/allClasses/page.js
@@ -9,12 +9,15 @@ const AllClasses = () => {
             <div className='grid grid-cols-3 gap-5'>
                 {
                     classes.map(cls => <div key={cls} className="card bg-base-100 shadow-xl">
-                        <figure><Image className="w-full h-[350px]" src={demoPic} alt="Shoes" /></figure>
+                        <figure className='overflow-hidden'><Image className="w-full h-[350px] scale-110" src={demoPic} alt="Shoes" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <h2 className="card-title">React fundamentals</h2>
+                            <h5 className='text-[14px] mb-2'>Joy mahmud</h5>
+                            <p>Master the core concepts of React.js and build powerful user interfaces efficiently with this comprehensive course.</p>
+                            <h2 className='text-[18px]'>Total Enrollment:0</h2>
+                            <div className="card-actions justify-between items-center">
+                                <h3 className='text-xl font-semibold'>Price:<span> $120</span></h3>
+                                <button className="btn btn-primary">Enroll now</button>
                             </div>
                         </div>
                     </div>)
