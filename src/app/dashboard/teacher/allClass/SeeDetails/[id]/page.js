b5a6b765b5afd63630c5c0694f811e77946a2052
joy-mahmud <joymahmudiucse@gmail.com>
2024-02-27 11:26:18 +0600

    teacher dashboard see details delete update routes completed

--- src/app/dashboard/teacher/allClass/SeeDetails/[id]/page.js
+++ src/app/dashboard/teacher/allClass/SeeDetails/[id]/page.js
@@ -4,7 +4,7 @@ const SeeDetails = ({ params }) => {
     return (
         <div className='mx-5'>
             <h2>see details about the class no:{params.id}</h2>
-            <div className=' grid grid-cols-3 gap-3'>
+            <div className='grid grid-cols-3 gap-3'>
                 <div class="h-[200px] bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-2xl flex justify-center items-center text-white">
                     <h2 className='text-3xl font-bold '>Total Enrollment: 0 </h2>
                 </div>
